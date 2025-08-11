c937c442dc626019e32e3ddecdd0811e09035b06
Angelo Domingo <angelo.domingo118@gmail.com>
2025-08-11 10:24:17 +0800

    Replace edit icon with pencil icon in employee and division index views for improved UI consistency

--- resources/views/livewire/admin/data/employees-and-divisions/employees/index.blade.php
+++ resources/views/livewire/admin/data/employees-and-divisions/employees/index.blade.php
@@ -426,7 +426,7 @@ new #[Layout('components.layouts.app')] class extends Component {
                             </td>
                             <td class="whitespace-nowrap {{ $densityClasses['table_cell'] }} text-right {{ $densityClasses['text_base'] }} font-medium">
                                 <button wire:click="editEmployee({{ $employee->id }})" class="inline-flex items-center rounded-md border border-stone-300 bg-white px-2.5 py-1.5 {{ $densityClasses['text_base'] }} font-semibold text-stone-900 shadow-sm hover:bg-stone-50 dark:border-stone-700 dark:bg-stone-800 dark:text-stone-200 dark:hover:bg-stone-700/50">
-                                   <x-flux::icon.edit class="mr-1.5 h-4 w-4" />
+                                   <x-flux::icon.pencil class="mr-1.5 h-4 w-4" />
                                    Edit
                                 </button>
                             </td>
